b1f7c596ca0f0d08f1818a67fc8f90a37ed5a8df
2br-dev <masterkadaj@gmail.com>
2025-04-24 12:47:18 +0300

    Корректировки 24-04-2025

--- src/ts/master.ts
+++ src/ts/master.ts
@@ -70,7 +70,7 @@ let isScrollingRight:boolean;
 
 function clearActive(){
 	document.querySelector('.members')?.classList.remove('animated');
-	document.querySelectorAll('.members').forEach(el => {
+	document.querySelectorAll('.member').forEach(el => {
 		(el as HTMLElement).classList.remove('active');
 	})
 }
@@ -185,15 +185,16 @@ function initCounter()
 	}
 
 	// Вычисляем количество дней, часов, минут и секунд до достижения цели
-	var days = Math.floor(timeDiff / (1000 * 3600 * 24));
-	var hours = Math.floor((timeDiff % (1000 * 3600 * 24)) / (1000 * 3600));
-	var minutes = Math.floor((timeDiff % (1000 * 3600)) / 60000);
-	var seconds = Math.floor((timeDiff % 60000) / 1000);
+	let days = Math.floor(timeDiff / (1000 * 3600 * 24));
+	let daysStr = num_word(days, ['день', 'дня', 'дней']);
+	let hours = Math.floor((timeDiff % (1000 * 3600 * 24)) / (1000 * 3600));
+	let minutes = Math.floor((timeDiff % (1000 * 3600)) / 60000);
+	let seconds = Math.floor((timeDiff % 60000) / 1000);
 
-	let daysEl = <HTMLElement>document.querySelector('#ds');
-	let hoursEl = <HTMLElement>document.querySelector('#hr');
-	let minutesEl = <HTMLElement>document.querySelector('#mn');
-	let secondsEl = <HTMLElement>document.querySelector('#sc');
+	let daysEl = <HTMLElement>document.querySelector('#ds .num');
+	let hoursEl = <HTMLElement>document.querySelector('#hr .num');
+	let minutesEl = <HTMLElement>document.querySelector('#mn .num');
+	let secondsEl = <HTMLElement>document.querySelector('#sc .num');
 
 	daysEl.textContent = addZero(days);
 	hoursEl.textContent = addZero(hours);
@@ -201,6 +202,15 @@ function initCounter()
 	secondsEl.textContent = addZero(seconds);
 
 	requestAnimationFrame(initCounter);
+}
+
+function num_word(value:number, words:string[]){
+	value = Math.abs(value) % 100; 
+	var num = value % 10;
+	if(value > 10 && value < 20) return words[2]; 
+	if(num > 1 && num < 5) return words[1];
+	if(num == 1) return words[0]; 
+	return words[2];
 }
 
 function addZero(n: number): string {
